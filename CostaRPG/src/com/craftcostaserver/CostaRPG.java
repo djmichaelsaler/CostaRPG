6ec3527f57f65e1d14e1f311f9c189809c44759b
djmichaelsaler <djmichaelsaler@hotmail.com>
2014-04-09 01:07:56 +0200

    fddfgfdg

---
+++ CostaRPG/src/com/craftcostaserver/CostaRPG.java
@@ -0,0 +1,15 @@
+package com.craftcostaserver;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CostaRPG extends JavaPlugin{
+	
+	public void onEnable(){
+		getLogger().info("onEnable se ejecuto correctamente!");
+	}
+ 
+	public void onDisable(){
+		getLogger().info("onDisable se ejecuto correctamente!");
+	}
+	
+}
